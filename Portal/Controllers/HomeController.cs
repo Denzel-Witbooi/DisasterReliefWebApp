8568578b6bba0c82009253124fe38f3b194efe46
Denzel <dwitbooi41@gmail.com>
2022-11-05 17:39:35 +0200

    Add Donated Goods Statistics to About Page

--- Portal/Controllers/HomeController.cs
+++ Portal/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Portal.Data;
 using Portal.Models;
+using Portal.Models.ReliefViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -13,10 +16,11 @@ namespace Portal.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly DisasterReliefContext db;
+        public HomeController(ILogger<HomeController> logger, DisasterReliefContext disasterReliefContext)
         {
             _logger = logger;
+            db = disasterReliefContext;
         }
 
 
@@ -25,9 +29,30 @@ namespace Portal.Controllers
             return View();
         }
 
-        public IActionResult About()
+        public async Task<ActionResult> About()
         {
-            return View();
+            /*
+             * The LINQ statement groups the student entities 
+             * by enrollment date, calculates the number of entities 
+             * in each group, and stores the results in a 
+             * collection of EnrollmentDateGroup view model objects.
+             */
+            IQueryable<DonationGroup> data =
+                from good in db.Goods
+                group good by good.DonationDate into
+                dateGroup
+                select new DonationGroup()
+                {
+                    DonationDate = dateGroup.Key,
+                    GoodsCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
